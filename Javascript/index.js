ea02adc07ef580af982c0946d03010e80b7f16da
kk <517412663@qq.com>
2017-05-07 23:29:25 +0800

    添加每秒修改轮播图片的值

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,30 +1,45 @@
 new Vue({
 	el:".top",
 	data:{
+		pic:[],
+        
+        now:""
 		// imgs[]
 	},
 	mounted:function(){
 		this.$nextTick(function(){
 			this.getPic();
-
 		});
 	},
 	computed:{},
 	methods:{
 		getPic:function(){
 		// 也可以通过 params 对象传递参数
-		axios.get('Data/goPics.json', {
-		params: {
-		//ID: 12345
-		},
-		 responseType: 'json', // default
-		})
-		.then(function (response) {
-		console.log(response);
-		})
-		.catch(function (error) {
-		console.log(error);
-		});
+			axios.get('Data/goPics.json', {
+				params: {
+				//ID: 12345
+				},
+			 	responseType: 'json', // default
+			})
+			.then(function (response) {
+				this.pic=response;
+			let i=0;
+				//alert(this.pic.data[0].url)
+ 				 var _this=this;
+	 			setInterval(function(){
+			      
+			        if(i>=_this.pic.data.length){
+			        i=0;
+		       		}
+		       		 _this.now=_this.pic.data[i].url;
+		       		//Vue.set(_this.now, i,_this.pic.data[i].url);
+		       		console.log(_this.now);
+		       		 i++;
+		     },1000);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 		}
 	}
 });
